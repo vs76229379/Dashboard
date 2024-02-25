f0600a3e485e80002b6f9f449fa422fcb1247a1f
vs76229379 <104732165+vs76229379@users.noreply.github.com>
2024-02-25 12:38:03 +0530

    cors update1

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","https://dashboard-backend-w6lg.onrender.com")
+  res.setHeader("Access-Control-Allow-Origin","https://dashboard-frontend-viveksingh.onrender.com")
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
